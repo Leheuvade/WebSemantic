2bac5dcdfcb8af9b850b206a8ff213f02f08374c
handroid <p.anh96@gmail.com>
2017-11-19 19:19:48 +0100

    comment similarity

--- src/fr/insalyon/Similarity.java
+++ src/fr/insalyon/Similarity.java
@@ -1,139 +1,62 @@
 
 package fr.insalyon;
-import jdk.nashorn.internal.parser.JSONParser;
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.text.ParseException;
 import java.util.*;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 
 public class Similarity {
-
-    /*JSONObject obj = new JSONObject("{\n" +
-            "   \"pageInfo\": {\n" +
-            "         \"pageName\": \"abc\",\n" +
-            "         \"pagePic\": \"http://example.com/content.jpg\"\n" +
-            "    }\n" +
-            "    \"posts\": [\n" +
-            "         {\n" +
-            "              \"post_id\": \"123456789012_123456789012\",\n" +
-            "              \"actor_id\": \"1234567890\",\n" +
-            "              \"picOfPersonWhoPosted\": \"http://example.com/photo.jpg\",\n" +
-            "              \"nameOfPersonWhoPosted\": \"Jane Doe\",\n" +
-            "              \"message\": \"Sounds cool. Can't wait to see it!\",\n" +
-            "              \"likesCount\": \"2\",\n" +
-            "              \"comments\": [],\n" +
-            "              \"timeOfPost\": \"1234567890\"\n" +
-            "         }\n" +
-            "    ]\n" +
-            "}\n");
-    String pageName = obj.getJSONObject("pageInfo").getString("pageName");
-    System.out.println(pageName);
-    package fr.insalyon;*/
-
-    public int similarity(JSONArray jsonObject, JSONArray jsonObject2) {
-
-
-
-
-
-        //test afficher le champ p du premier element
-        // System.out.println(obj.get("p"));
+    /**
+    Compute Jaccard similarity between two graphs based on the p and o links
+     @param graph1,graph2
+     @return int percentage of similarity
+     */
+    public int similarity(JSONArray graph1, JSONArray graph2) {
 
         //initialisation compteur
         int compteur = 0;
-        //List<List<JSONObject>> union;
-        int longuer1, longueur2;
+        //Hashmap qui contient les triplets s,p,o
         Set<JSONObject> triplet = new HashSet();
-           /* Set<JSONObject> doublon = new HashSet();
-            Set<JSONObject> doublon2=new HashSet();*/
 
+        // pour chaque triplet du graphe 1 on cherche si les liens p et o sont contenu dans le graphe 2
+        for (int i = 0; i < graph1.length(); i++) {
 
-        //test afficher les values des champ p de tous les elements
-        for (int i = 0; i < jsonObject.length(); i++) {
-
-            JSONObject lien = (JSONObject) jsonObject.get(i);
+            JSONObject lien = (JSONObject) graph1.get(i);
             triplet.add(lien);
-            //JSONObject lienPO=new JSONObject();
 
-
-            JSONObject champS = (JSONObject) lien.get("s");
             JSONObject champP = (JSONObject) lien.get("p");
             JSONObject champO = (JSONObject) lien.get("o");
-            Object valueS = champS.get("value");
             Object valueP = champP.get("value");
             Object valueO = champO.get("value");
 
-                /*lienPO.put(champP,valueP);
-                lienPO.put(champO,valueO);
+            for (int j = 0; j < graph2.length(); j++) {
 
-                doublon.add(lienPO);*/
-            for (int j = 0; j < jsonObject2.length(); j++) {
-
-                JSONObject lien2 = (JSONObject) jsonObject2.get(j);
-                JSONObject champ2S = (JSONObject) lien2.get("s");
+                JSONObject lien2 = (JSONObject) graph2.get(j);
                 JSONObject champ2P = (JSONObject) lien2.get("p");
                 JSONObject champ2O = (JSONObject) lien2.get("o");
-                Object value2S = champ2S.get("value");
                 Object value2P = champ2P.get("value");
                 Object value2O = champ2O.get("value");
 
+                //incrementation du compteur lorsque les liens p et o des deux graphes sont égaux
                 if (valueP.equals(value2P) && valueO.equals(value2O)) {
                     compteur++;
-
-
                 }
-
             }
-
-
         }
 
-        for (int j = 0; j < jsonObject2.length(); j++) {
-
-            JSONObject lien2 = (JSONObject) jsonObject2.get(j);
-
-            //JSONObject lienPO2=new JSONObject();
-            JSONObject champ2P = (JSONObject) lien2.get("p");
-            JSONObject champ2O = (JSONObject) lien2.get("o");
-
-            Object value2P = champ2P.get("value");
-            Object value2O = champ2O.get("value");
-
-                /*lienPO2.put(champ2P,value2P);
-                lienPO2.put(champ2O,value2O);
-                doublon2.add(lienPO2);*/
-
+        //Union des deux graphes, le HashMap triplet contient tous les triplets s,p,o des deux graphes uniques
+        for (int j = 0; j < graph2.length(); j++) {
+            JSONObject lien2 = (JSONObject) graph2.get(j);
             if (triplet.contains(lien2) == false) {
                 triplet.add(lien2);
             }
-                /*if (doublon.contains(lienPO2) == false)
-                {
-                    doublon.add(lienPO2);
-                }*/
+
         }
 
         System.out.println(compteur);
-        //System.out.println("Liste intiitiale  1 size: "+jsonObject.size());
-        // System.out.println("triplet liste 1 size: "+triplet.size());
-        //System.out.println("Liste intiitiale  2 size: "+jsonObject2.size());
-        //System.out.println("doublons liste 2 size: "+doublon2.size());
+
         System.out.println("Degré de similarité: ");
         return 100 * compteur / triplet.size();
-
-/*
-            String name = (String) jsonObject.get("prenom");
-            String name2 = (String) jsonObject2.get("prenom");
-
-            if(name.equals(name2))
-            {
-            System.out.println("Name: " + name);
-                System.out.println("Name: " + name2);}*/
-
-
 
 
     }
